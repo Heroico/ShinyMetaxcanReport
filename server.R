538af0ae13e003dcde485e2607ce3d20c21b8091
heroico <soyunheroe@gmail.com>
2016-05-05 12:24:26 -0300

    Will display phenotype list or results

--- server.R
+++ server.R
@@ -1,23 +1,40 @@
 source("helpers.R", local=TRUE)
 source("phenoInfoModule.R", local=TRUE)
 
+selection_event <- function(selected, index) {
+    info <- NULL
+    if (length(selected) && length(index)) {
+      selected_row = as.numeric(tail(selected, 1))
+      selected_pheno_key <- index[as.numeric(selected_row)]
+      info <- pheno_info_with_tag(p, selected_pheno_key)
+    }
+    info
+}
 
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   # Filter data based on selections
   selected_pheno <- ""
-  rows_index <- data.frame()
+  results_index <- data.frame()
+  pheno_index <- data.frame()
   selected_index <- -1
 
-#  output$pheno <- DT::renderDataTable({
-#    l <- p[,names(p) %in% c("tag", "name", "population", "consortium", "sample_size")]
-#    DT::datatable(l,selection = 'single',escape = FALSE)
-#  })
+  output$pheno <- DT::renderDataTable({
+    l <- p[,names(p) %in% c("tag", "name", "population", "consortium", "sample_size")]
+    pheno_index <<- l[,"tag"]
+    DT::datatable(l,
+      options = list(
+        pageLength = 20,
+        lengthMenu = c(10, 20, 40, 100)
+      ),
+    selection = 'single'
+    )
+  })
 
   output$results <- DT::renderDataTable({
     data <- get_results_data_from_db(input)
     if ( length(data) ){
-        rows_index <<- data[,"phenotype"]
+        results_index <<- data[,"phenotype"]
     }
     DT::datatable(data,
     options = list(
@@ -30,21 +47,29 @@ shinyServer(function(input, output) {
 # Used for debugging, mostly
 #  proxy <- DT::dataTableProxy('results')
 
-  modal_process <- eventReactive(input$results_rows_selected, {
-    s = input$results_rows_selected
-    info <- NULL
-    if (length(s) && length(rows_index)) {
-      selected_row = as.numeric(tail(s, 1))
-      selected_pheno_key <- rows_index[as.numeric(selected_row)]
-      info <- pheno_info_with_tag(p, selected_pheno_key)
+#  modal_process <- eventReactive(input$results_rows_selected, {
+#    selection_event(input$results_rows_selected, results_index)
+#  })
+
+  modal_process <- reactive({
+    v <- NULL
+    if (input$display == "results") {
+      v <- selection_event(input$results_rows_selected, results_index)
+    } else if(input$display == "pheno") {
+      v <- selection_event(input$pheno_rows_selected, pheno_index)
     }
-    info
+    v
   })
 
   output$phenoInformation <- renderUIPhenoInformation(modal_process)
 
   observeEvent(
     input$results_rows_selected,{
+    shinyjs::runjs("$('#myModal').modal()")
+  })
+
+  observeEvent(
+    input$pheno_rows_selected,{
     shinyjs::runjs("$('#myModal').modal()")
   })
 
--- ui.R
+++ ui.R
@@ -4,7 +4,8 @@ source("helpers.R")
 sgt <- c("All", t$tag)
 sgp <- c("All", p$tag)
 
-build_ui <- function(){
+# Not used at the moment, but might come back
+build_ui_d <- function(){
   f <- fluidPage(
     shinyjs::useShinyjs(),
     titlePanel("Metaxcan Association results"),
@@ -29,6 +30,45 @@ build_ui <- function(){
     #fluidRow(
     #  DT::dataTableOutput(outputId="pheno")
     #)
+  )
+  f
+}
+
+build_ui <- function(){
+  f <- fluidPage(
+    shinyjs::useShinyjs(),
+    titlePanel("Metaxcan Association results"),
+    htmlTemplate("modal.html",
+      title = "Phenotype Information",
+      content = uiOutput("phenoInformation")
+    ),
+    p("'Tissue' stands for different transcriptome models used when generating the association."),
+    p("Tissues built with GTEX data, covariances with 1000 Genomes."),
+    selectInput(
+      "display", "What to show:",
+      c(Results = 'results',
+        Phenotypes = 'pheno')),
+    conditionalPanel(
+      condition = "input.display == 'results'",
+      fluidRow(
+        column(2, textInput("gene_name", "Gene Name:","")),
+        column(1, checkboxInput("ordered", label = "Ordered", value = TRUE)),
+        column(2, selectInput("pheno", "Phenotype:", sgp)),
+        column(2, selectInput("tissue", "Tissue:", sgt)),
+        column(1, numericInput("r2_threshold", "R2 threshold:",0.01)),
+        column(1, numericInput("threshold", "Pvalue threshold:",0.05)),
+        column(1, numericInput("limit", "Record limit:", 100))
+      ),
+      fluidRow(
+        DT::dataTableOutput(outputId="results")
+      )#,
+    ),
+    conditionalPanel(
+      condition = "input.display == 'pheno'",
+      fluidRow(
+        DT::dataTableOutput(outputId="pheno")
+      )
+    )
   )
   f
 }
